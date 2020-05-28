d19749d442b5268935815c3af6a1c35ad8348607
Armagan Amcalar <armagan@amcalar.com>
2020-05-28 22:36:09 +0200

    Add initial frontend files

---
+++ week-8/frontend/src/views/About.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'About',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template lang="pug">
+  .about
+    h1 This is an about page
+    HelloWorld(msg="This is Istanbul!")
+</template>
---
+++ week-8/frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
